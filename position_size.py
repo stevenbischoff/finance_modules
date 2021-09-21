bae213c1e839f5c6efd4dcb33c599f55b55e5334
Steven Bischoff <stevenbischoff1@gmail.com>
2021-09-21 14:44:07 -0500

    clean, add
    clean sizing_returns, date_returns
    add max_drawdown

--- position_size.py
+++ position_size.py
@@ -20,19 +20,26 @@ def optimal_returns(returns1, returns2, start_date, end_date):
 def sizing_returns(returns1, returns2, i_temp, j_temp, start_date, end_date):
 
   delta = dt.timedelta(days=1)
-  bank = 1
+  bank_series = pd.Series()
 
   while start_date <= end_date:
-    bank = date_return(returns1, returns2, i_temp, j_temp, start_date, bank)
+    date_return(returns1, returns2, i_temp, j_temp, start_date, bank_series)    
     start_date += delta
+
+  bank = bank_series.product()
 
   return bank
 
 
-def date_return(returns1, returns2, i_temp, j_temp, date, bank):
+def date_return(returns1, returns2, i_temp, j_temp, date, bank_series):
 
   returns1_date = returns1.loc[returns1['Date'] == date, 'profit']
   returns2_date = returns2.loc[returns2['Date'] == date, 'profit']
+
+  n_open1, n_open2 = len(returns1_date), len(returns2_date)
+
+  if n_open1 == 0 and n_open2 == 0:
+    return
 
   if len(returns1_date) > 0:
     returns1_date_total = i_temp*len(returns1_date)*(returns1_date.mean() - 1)
@@ -43,6 +50,11 @@ def date_return(returns1, returns2, i_temp, j_temp, date, bank):
   else:
     returns2_date_total = 0
 
-  bank *= 1 + returns1_date_total + returns2_date_total
+  bank_series[date] = 1 + returns1_date_total + returns2_date_total
+  
 
-  return bank 
+def max_drawdown(cumulative_bank_series):
+  drawdown_series = cumulative_bank_series/cumulative_bank_series.cummax() - 1
+  max_drawdown = drawdown_series.min()
+  
+  return max_drawdown
